33cbab8155c09ce0d71ab754c3dfc51470dc6c54
info <nasihz12345@gmail.com>
2024-07-19 23:51:16 -0400

    ss

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 function calculateTotalCost() {
-    var distance = parseFloat(document.getElementById('distance').value);
-    var fuelCost = parseFloat(document.getElementById('fuelCost').value);
-    var averageMPG = parseFloat(document.getElementById('averageMPG').value);
-    var cargoWeight = parseFloat(document.getElementById('cargoWeight').value);
-    var driverWage = parseFloat(document.getElementById('driverWage').value);
-    var travelHours = parseFloat(document.getElementById('travelHours').value);
+  //  date
+    // comapny
 
-    var fuelNeeded = distance / averageMPG;
-    var totalFuelCost = fuelNeeded * fuelCost;
-    var totalDriverCost = driverWage * travelHours;
 
-    var totalCost = totalFuelCost + totalDriverCost;
-    document.getElementById('result').innerText = 'Total Cost: $' + totalCost.toFixed(2);
+    var totalMiles = parseFloat(document.getElementById('distance').value);
+    var payRate = parseFloat(document.getElementById('fuelCost').value);
+    var totalpayRate= parseFloat(document.getElementById('fuelCost').value);
+    var Pickup_drop = parseFloat(document.getElementById('averageMPG').value);
+    var waiting_Time = parseFloat(document.getElementById('cargoWeight').value);
+    var Safety_bonus = parseFloat(document.getElementById('driverWage').value);
+
+    var layOver = parseFloat(document.getElementById('travelHours').value);
+    var    Cash_Advance = parseFloat(document.getElementById('travelHours').value);
+
+    var Driver_pay = parseFloat(document.getElementById('travelHours').value);
+ var Misc =parseFloat(document.getElementById('travelHours').value);
+    var totalFuelCost = parseFloat(document.getElementById('travelHours').value);
+   var  pay_recieved = parseFloat(document.getElementById('travelHours').value);
+  var  comapny_profit = parseFloat(document.getElementById('travelHours').value);
+
+
+
+    var totalpayrate = totalMiles * payRate
+    document.getElementById('result').innerText = 'Total Cost: $' + totalpayrate.toFixed(2);
 }
